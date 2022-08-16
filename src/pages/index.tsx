f81a005cf03b895fa7ba25bb763015ae75f60c6c
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-08-17 01:16:43 +0900

    🎉

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,6 @@ const Home: CustomNextPage = () => {
   return (
     <div className="p-20">
       <h1 className="mb-10">Hello World!!</h1>
-      <Link href="/start">
-        <a className="border bg-slate-700 p-2 text-white">start!!</a>
-      </Link>
-      <Link href={pagesPath.add.$url()}>
-        <a className="border bg-slate-700 p-2 text-white">add!!</a>
-      </Link>
-      <Link href={pagesPath.calculation.$url()}>
-        <a className="border bg-slate-700 p-2 text-white">calculation!!</a>
-      </Link>
-      <Link href={pagesPath.signin.$url()}>
-        <a className="border bg-slate-700 p-2 text-white">signin!!</a>
-      </Link>
-      <Link href={pagesPath.signup.$url()}>
-        <a className="border bg-slate-700 p-2 text-white">singup!!</a>
-      </Link>
     </div>
   );
 };
